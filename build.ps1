a7bf8d160ed6e303c8e94896825597887b6e25de
Adam Wu <adamwu@Adams-MacBook-Pro.local>
2020-03-12 20:53:24 +1100

    Add Dockerfile

--- build.ps1
+++ build.ps1
@@ -1,2 +1,4 @@
 dotnet restore webapp/webapp.csproj
-dotnet build webapp/webapp.csproj
+dotnet publish webapp/webapp.csproj -o webapp/publish
+docker build -t webapp -f webapp/Dockerfile
+docker images
